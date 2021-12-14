537b0797683184053177c42e0e899eedc2979cfb
Giuseppe <giumic@gmail.com>
2021-12-14 16:46:15 +0100

    Aggiungere i file di progetto.

---
+++ Biblioteca/Autore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Autore : Persona
+    {
+
+        public Autore(string Nome, string Cognome) : base(Nome, Cognome)
+        {
+
+        }
+    }
+
+}
---
+++ Biblioteca/Biblioteca.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    class Biblioteca
+    {
+        public string Nome { get; set; }
+        public List<Documento> Documenti { get; set; }
+        public List<Prestito> Prestiti { get; set; }
+        public List<Utente> Utenti { get; set; }
+
+        public Biblioteca(string Nome)
+        {
+            this.Nome = Nome;
+            this.Documenti = new List<Documento>();
+            this.Prestiti = new List<Prestito>();
+            this.Utenti = new List<Utente>();
+        }
+
+        public List<Documento> SearchByCodice(string Codice)
+        {
+            return this.Documenti.Where(d => d.Codice == Codice).ToList();
+        }
+
+        public List<Documento> SearchByTitolo(string Titolo)
+        {
+            return this.Documenti.Where(d => d.Titolo == Titolo).ToList();
+        }
+
+        public List<Prestito> SearchPrestiti(string Numero)
+        {
+            return this.Prestiti.Where(p => p.Numero == Numero).ToList();
+        }
+
+        public List<Prestito> SearchPrestiti(string Nome, string Cognome)
+        {
+            return this.Prestiti.Where(p => p.Utente.Nome == Nome && p.Utente.Cognome == Cognome).ToList();
+        }
+    }
+
+    enum Stato { Disponibile, Prestito }
+    
+
+}
